4a4e1700fb9358d51dc087938b7f94644aaf8cdf
srduardo <eduardodev8@gmail.com>
2024-10-06 02:07:24 -0300

    Updated: Desenvolvimento da janela de registro de produtos iniciado

---
+++ frontend/js/script.js
@@ -0,0 +1,9 @@
+const registerBtn = document.getElementById("register-button");
+
+registerBtn.addEventListener("click", function() {
+    const registerWindow = document.createElement("form");
+    const divNome = document.createElement("div");
+
+
+
+});
